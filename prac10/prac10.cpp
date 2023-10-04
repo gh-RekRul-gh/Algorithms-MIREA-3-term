dcb6088137adfdaeb1d427ac637567eacabc3532
gh-RekRul-gh <rerusya18@mail.ru>
2023-10-05 02:50:02 +0300

    prac10 added

---
+++ prac10/prac10.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void printReversedString(string& str, int cur) {
+    if (cur == str.size() - 1) {
+        cout << str[cur];
+        return;
+    }
+    printReversedString(str, cur + 1);
+    cout << str[cur];
+    return;
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+    string str;
+    cout << "Введите строку\n";
+    getline(cin, str);
+    cout << "Перевернутая строка:\n";
+    printReversedString(str, 0);
+    return 0;
+}
